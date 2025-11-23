ac21718dc7807eaefd75c1e43c8592a5efc12ed0
youssef hachimi <83027476+youssef-hachimi@users.noreply.github.com>
2025-11-23 21:56:15 +0100

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,13 @@
+import math
+# x = 9.1
+# result = math.ceil(x)
+# result = math.floor(x)
+# result = math.sqrt(x)
+# result = math.cos(x)
+# print(result)
+
+# calculate the circumference of a circle :
+# c = 2 pi r
+radius = float(input('enter the redius of a circle:'))
+circumference = 2 * math.pi * radius
+print(f"The circumference of this circle is {circumference}")
